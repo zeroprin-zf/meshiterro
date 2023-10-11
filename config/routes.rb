2628d554d4feca533a8b01ca74e73e3e037e0965
zeroprin-zf <82091884+zeroprin-zf@users.noreply.github.com>
2023-10-11 08:05:26 +0000

    [Finish]4-6

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'homes/top'
+  get 'homew/about' 
   root to: "homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
