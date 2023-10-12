e1e40120a317d42309b4743e4dda88a0eff94bc6
zeroprin-zf <82091884+zeroprin-zf@users.noreply.github.com>
2023-10-12 05:40:11 +0000

    [Finish]4-13

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
 
   resources :post_images, only: [:new, :create, :index, :show]
   get 'homes/about' => 'homes#about', as:'about'
-  #root to: "homes#top"
+  root to: "homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
