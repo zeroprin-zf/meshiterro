83ddab5cf20d54139260dbb1423a12619118c654
zeroprin-zf <82091884+zeroprin-zf@users.noreply.github.com>
2023-10-13 02:33:44 +0000

    [Finish]4-19

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'homes/top'
 
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
   resources :users, only: [:show, :edit, :update]
