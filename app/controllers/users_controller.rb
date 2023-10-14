3c14ba0c6bcd36695c49b3e2feba904539ae9134
zeroprin-zf <82091884+zeroprin-zf@users.noreply.github.com>
2023-10-14 07:04:42 +0000

    ほかのユーザーからのアクセス制限

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
     @post_images = @user.post_images.page(params[:page])
   end
 
   def edit
-    @user = User.find(params[:id])
+     @user = User.find(params[:id])
   end
 
   def update
@@ -18,5 +20,12 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :profile_image)
+  end
+
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to post_images_path
+    end
   end
 end
